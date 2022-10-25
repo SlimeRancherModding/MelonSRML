11228ada9566491ad74a3fff4f2786398e6c78c1
PickleOnAString <69421001+PickleOnAString@users.noreply.github.com>
2022-10-26 01:46:29 +1100

    added some utils

--- MelonSRML/Utils/TextureUtility.cs
+++ MelonSRML/Utils/TextureUtility.cs
@@ -65,5 +65,31 @@ namespace MelonSRML.Utils
             if (!Directory.Exists(folderToSaveTo)) Directory.CreateDirectory(folderToSaveTo);
             File.WriteAllBytes(folderToSaveTo + texture.name + ".png", bytes);
         }
+
+        /// <summary>
+        /// Creates A Texture Ramp
+        /// </summary>
+        public static Texture2D CreateRamp(Color a, Color b, params Color[] others)
+        {
+            Texture2D texture2D = new Texture2D(128, 32);
+            List<Color> colorList = new List<Color>
+            {
+                a,
+                b
+                };
+            colorList.AddRange(others);
+            int num = Mathf.RoundToInt(128f / (colorList.Count - 1));
+            for (int x = 0; x < 128; ++x)
+            {
+                Color color = Color.Lerp(colorList[0], colorList[1], x % num / (num - 1));
+                if (x % num == num - 1)
+                    colorList.RemoveAt(0);
+                for (int y = 0; y < 32; ++y)
+                    texture2D.SetPixel(x, y, color);
+            }
+            texture2D.name = string.Format("generatedTexture-{0}", texture2D.GetInstanceID());
+            texture2D.Apply();
+            return texture2D;
+        }
     }
 }

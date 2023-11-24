f21d02312e61017b98e9166197994411aee3d8c6
MadMincraftr <thean.mcgarity@gmail.com>
2023-11-23 21:39:34 -0600

    Fix spawn command

--- MelonSRML/Console/Commands/SpawnCommand.cs
+++ MelonSRML/Console/Commands/SpawnCommand.cs
@@ -15,7 +15,7 @@ namespace MelonSRML.Console.Commands
 
         public override bool Execute(string[] args)
         {
-            if(args.Length < 1 || args.Length > 2)
+            if (args.Length < 1 || args.Length > 2)
             {
                 MelonLogger.Error("Incorrect number of arguments!");
                 return false;
@@ -37,13 +37,13 @@ namespace MelonSRML.Console.Commands
 
             if (args.Length != 2 || !int.TryParse(args[1], out count)) count = 1;
 
-            for(int i = 0; i < count; i++)
+            for (int i = 0; i < count; i++)
             {
                 if (Physics.Raycast(new Ray(Camera.main.transform.position, Camera.main.transform.forward), out var hit))
                 {
                     var spawned = SRBehaviour.InstantiateActor(prefab, SceneContext.Instance.RegionRegistry.CurrentSceneGroup, Vector3.zero, Quaternion.identity);
                     if (spawned.GetComponent<Collider>() != null)
-                        spawned.transform.position = hit.point+hit.normal*PhysicsUtil.CalcRad(spawned.GetComponent<Collider>());
+                        spawned.transform.position = hit.point + hit.normal * PhysicsUtil.CalcRad(spawned.GetComponent<Collider>());
                     var delta = -(hit.point - Camera.main.transform.position).normalized;
                     spawned.transform.rotation = Quaternion.LookRotation(delta, hit.normal);
                 }
@@ -54,8 +54,8 @@ namespace MelonSRML.Console.Commands
 
         public override List<string> GetAutoComplete(int argIndex, string argText)
         {
-            
-            if(argIndex == 0)
+
+            if (argIndex == 0)
             {
                 return Resources.FindObjectsOfTypeAll<IdentifiableTypeGroup>().FirstOrDefault(x => x.name.Equals("VaccableNonLiquids"))?.GetAllMembers().ToArray().Select(x => x.ValidatableName).ToList();
             }
